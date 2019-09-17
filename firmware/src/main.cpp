7723a6590aaf0e531ded80786ed4a13098e339db
robertodebarba <roberto.debarba@gmail.com>
2019-09-17 19:50:15 -0300

    Alterado firmware para suportar replace

--- firmware/src/main.cpp
+++ firmware/src/main.cpp
@@ -11,6 +11,46 @@ const int FW_VERSION = 0; //Unix Timestamp
 const char *fwUrlBase = "https://vpl-esp8266.s3.amazonaws.com/";
 const char *fingerprint = "17:E0:A9:3E:58:AF:0A:06:8D:6C:2D:B6:C1:80:B3:E7:E3:52:D4:8E";
 
+const char *wifiSSID = "Redmi";
+const char *wifiPassword = "qwert12345";
+
+void checkForUpdates();
+
+//@@REPLACE_GLOBAL_INIT@@
+
+void setup()
+{
+  Serial.begin(115200);
+
+  Serial.println();
+  Serial.println();
+  Serial.println();
+
+  for (uint8_t t = 4; t > 0; t--)
+  {
+    Serial.println("Seting up...");
+    delay(1000);
+  }
+
+  WiFi.mode(WIFI_STA);
+  WiFiMulti.addAP(wifiSSID, wifiPassword);
+
+  //@@REPLACE_SETUP@@
+}
+
+void loop()
+{
+  // wait for WiFi connection
+  if ((WiFiMulti.run() == WL_CONNECTED))
+  {
+    checkForUpdates();
+  }
+
+  delay(5000);
+
+  //@@REPLACE_LOOP@@
+}
+
 void checkForUpdates()
 {
   String fwURL = String(fwUrlBase);
@@ -69,31 +109,4 @@ void checkForUpdates()
   httpClient.end();
 }
 
-void setup()
-{
-  Serial.begin(115200);
-
-  Serial.println();
-  Serial.println();
-  Serial.println();
-
-  for (uint8_t t = 4; t > 0; t--)
-  {
-    Serial.println("Seting up...");
-    delay(1000);
-  }
-
-  WiFi.mode(WIFI_STA);
-  WiFiMulti.addAP("Redmi", "qwert12345");
-}
-
-void loop()
-{
-  // wait for WiFi connection
-  if ((WiFiMulti.run() == WL_CONNECTED))
-  {
-    checkForUpdates();
-  }
-
-  delay(5000);
-}
+//@@REPLACE_GLOBAL_FUNCTIONS@@
