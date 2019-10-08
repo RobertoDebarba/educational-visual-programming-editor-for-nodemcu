34dc8745093d22d19fe048a54d347960d71d9fde
robertodebarba <roberto.debarba@gmail.com>
2019-10-08 20:49:30 -0300

    Calibração motion_turndirection

--- frontend/custom_blocks/code_generator/motion_turndirection.js
+++ frontend/custom_blocks/code_generator/motion_turndirection.js
@@ -2,8 +2,7 @@ Blockly.JavaScript['motion_turndirection'] = function (block) {
     let angle_angle = block.getFieldValue('angle');
     let angle_direction = block.getFieldValue('direction');
 
-    let angle = angle_angle / 20; //TODO calcular quantos graus por passo
-    let direction = angle_direction === 'RIGHT' ? 1 : 0;
+    let angle = angle_angle / 6;
 
-    return `Otto.turn(${angle},1000,${direction});\n`;
+    return `Otto.turn(${angle},1000,${angle_direction});\n`;
 };
